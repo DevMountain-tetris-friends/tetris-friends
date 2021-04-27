496484b38701ca4b738033f0a05b84aa6d728b8f
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-27 16:29:21 -0600

    DB main structure outline. Ready for QA review

---
+++ tetris2/db/seed.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY, 
+  username VARCHAR(50),
+  first_name VARCHAR(50), 
+  last_name VARCHAR(50),
+  email VARCHAR(60),
+  password TEXT, 
+  highest_score INT(10)
+);
+
+CREATE TABLE scores (
+  score_id INT PRIMARY KEY,
+  user_id INT REFERENCES users(user_id) ON DELETE cascade, 
+  highest_score INT REFERENCES users(highest_score)
+)
