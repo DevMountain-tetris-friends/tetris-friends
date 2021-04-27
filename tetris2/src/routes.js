42b86fc09b3eb555f022c61c27e8a541bd8ff77f
TannerCooper97 <Tannerccooper#gmail.com>
2021-04-27 16:02:12 -0600

    Started basic structure of redux

--- tetris2/src/routes.js
+++ tetris2/src/routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import mainPage from './Components/mainPage/mainPage';
+import tetris from './Components/gameMarathon/tetris';
 
 
 
 export default(
     <Switch> 
-        
+        <Route component={mainPage} exact path ='/'/>
+        <Route component={tetris} path='/tetris' />
     </Switch>
 )
