bea96f325c79b7f27ea793c110b99f163bb40d10
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-29 11:56:47 -0600

    updating Login and Register components. Full Crud application

--- tetris2/src/Components/Login/Login.js
+++ tetris2/src/Components/Login/Login.js
@@ -1,22 +1,34 @@
-import React, { useState } from 'react';
-import {loginUserDispatch} from '../../redux/userService';
-import { useSelector } from 'react-redux';
-import {Redirect, Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import {loginUser} from '../../redux/userReducer';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import Register from './Register';
 
-function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+function Login(props) {
+    const [usernameInputText, setUsernameInputText] = useState('');
+    const [passwordInputText, setPasswordInputText] = useState('');
     const [register, setRegister] = useState(false);
+    const {user} = props;
+    const {push} = props.history;
 
-    function loginFunction() {
-        loginUserDispatch(username, password);
+    useEffect(() => {
+        if(user){
+            push('/')
+        }
+    }, [user, push]);
+    
+    const loginFunction = () => {
+        axios.post('/auth/login', {
+            username: '', 
+            password: '',
+        })
+        .then(res => {
+            console.log(res.data)
+            props.loginUser(res.data)
+        })
+        .catch(err => console.log(err))
     }
-
-    
-
-    console.log('got here');
-    
 
     return(
         <div>
@@ -26,9 +38,9 @@ function Login() {
 
             <div>
                 <p> username </p>
-                <input className='input-box' value={username} type='text' onChange={e => setUsername(e.target.value)}/>
+                <input className='input-box' value={usernameInputText} type='text' onChange={e => setUsernameInputText(e.target.value)}/>
                 <p> password </p>
-                <input className='input-box' value={password} type='password' onChange={ e => setPassword(e.target.value)}/>
+                <input className='input-box' value={passwordInputText} type='password' onChange={ e => setPasswordInputText(e.target.value)}/>
             </div>
             }
 
@@ -39,4 +51,10 @@ function Login() {
     )
     
 }
-export default Login;
+const mapStateToProps = (stateRedux) => {
+    return {
+        user: stateRedux.users.user
+    }
+}
+
+export default connect(mapStateToProps,{loginUser})(Login);
--- tetris2/src/redux/userService.js
+++
@@ -1,9 +0,0 @@
-import store from './store';
-import axios from 'axios';
-import { loginUser } from './userReducer';
-
-export function loginUserDispatch(username, password) {
-    let promise = axios.post('/auth/login', {username, password}).then(res => res.data)
-    store.dispatch(loginUser(promise))
-}
-
