c7b6f2fb3363955a1baff2f102f1882640b7e97f
Moses Kaumatule <kaumatule93@gmail.com>
2021-05-03 12:35:49 -0600

    updating merge conflict with community board

--- tetris2/src/Components/MainPage/MainPage.js
+++ tetris2/src/Components/MainPage/MainPage.js
@@ -8,9 +8,9 @@ import {
 import Modal from '../Modal/Modal'
 import User from '../User/User'
 import {connect} from 'react-redux'
-import CommunityBoard from '../CommunityBoard/CommunityBoard'
+// import CommunityBoard from '../CommunityBoard/CommunityBoard'
 
-import './MainPage.scss'
+import './MainPage.scss';
 
 function MainPage(props) {
     const {user, updateUser, history} = props
@@ -65,9 +65,9 @@ function MainPage(props) {
             <Modal ref={modalRef}>
                 <User modalRef={modalRef}/>
             </Modal>
-            <div>
+            {/* <div>
                 <CommunityBoard/>
-            </div>
+            </div> */}
         </div>
     )
     
