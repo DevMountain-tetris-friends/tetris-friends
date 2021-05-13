c6ce972e5737998ed39ce41f1c57b3b49a12754e
TannerCooper97 <Tannerccooper#gmail.com>
2021-05-13 13:09:01 -0600

    HardMode now implemented

---
+++ tetris2/src/Components/HardMode/HardModeTetris.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TetrisGame from './TetrisComponents/TetrisGame';
+
+const Tetris = () => (
+  <div className="Tetris">
+    <TetrisGame />
+  </div>
+);
+
+export default Tetris;
